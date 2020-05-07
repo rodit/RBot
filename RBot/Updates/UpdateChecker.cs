7fcd95a65f63982e14dc3e15f5de2e299c3fff8e
Rodi Degirmenci <rodit@live.co.uk>
2020-05-07 17:58:03 +0100

    Corrected update checker's GH api authentication.

--- RBot/Updates/UpdateChecker.cs
+++ RBot/Updates/UpdateChecker.cs
@@ -12,11 +12,11 @@ namespace RBot.Updates
 {
     public class UpdateChecker
     {
-        public const string ApiUrl = "https://api.github.com/repos/rodit/rbot/releases?client_id=726820423be5c752df62&client_secret=63b2a5b1a55fbeade88deab3b6c8914808bad7a6";
+        public const string ApiUrl = "https://api.github.com/repos/rodit/rbot/releases";
 
         public static async Task<List<UpdateInfo>> GetReleases()
         {
-            using (RBotWebClient wc = new RBotWebClient())
+            using (GHWebClient wc = new GHWebClient())
                 return JsonConvert.DeserializeObject<List<UpdateInfo>>(await wc.DownloadStringTaskAsync(ApiUrl));
         }
     }
